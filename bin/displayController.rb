02d091c72aa1c531e7eca8bdc79d8bc732195c32
newcoder171 <33447805+newcoder171@users.noreply.github.com>
2019-03-21 22:03:22 +0100

    Update

--- bin/displayController.rb
+++ bin/displayController.rb
@@ -4,7 +4,7 @@ class DisplayController
   # displays the game board
   # using the passed in list items as cell values
   def self.print_board(board)
-    %{  #{board[0]}|#{board[1]}|#{board[2]}
+   puts %{  #{board[0]}|#{board[1]}|#{board[2]}
   -+-+-
   #{board[3]}|#{board[4]}|#{board[5]}
   -+-+-
@@ -26,6 +26,22 @@ class DisplayController
     else
       puts "#{player}'s turn, pick a position between 1 - 9"
     end
+  end
+
+  def self.game_over(action, player = nil)
+    if player.nil?
+      if action.equal?('D')
+        puts 'DRAW!'
+      elsif action.equal?('G')
+        puts 'GAME OVER!'
+      end
+    else
+      puts "#{player} WINS"
+    end
+  end
+
+  def self.player_name(player)
+    puts "#{player} enter your name:"
   end
 
 end
